b66339613caffe8ea4d84d7245af58eca4ba1e82
osamax01 <shkukanios@gmail.com>
2024-02-16 15:42:14 +0300

    create todo app & add it to the project

--- DjangoLearning/urls.py
+++ DjangoLearning/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("hello/", include("hello.urls")),
     path("birthday/", include("birthday.urls")),
+    path("todo/", include("todo.urls")),
     path("admin/", admin.site.urls),
 ]
