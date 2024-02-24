a6b3cdd567c58be8d4864825bebf3dceaae75588
osamax01 <shkukanios@gmail.com>
2024-02-24 22:45:12 +0300

    users app: add index, login, and logout functionalities

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 
 from . import views
 
-url_patterns = [
+app_name = "users"
+urlpatterns = [
+    path("", views.index, name = "index"),
+    path("login", views.login_view, name = "login"),
+    path("logout", views.logout_view, name = "logout"),
 ]
