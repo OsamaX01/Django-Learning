4eb8958940d8b289f02d10196f3db0bed437baa2
osamax01 <shkukanios@gmail.com>
2024-02-24 17:57:40 +0300

    flights app: add book view and 'add passengers' form

--- flights/urls.py
+++ flights/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'flights'
 urlpatterns = [
     path("", views.index, name = "index"),
     path("<int:flight_id>", views.single_flight, name = "single_flight"),
+    path("book/<int:flight_id>", views.book, name = "book"),
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -14,8 +16,18 @@ def single_flight(request, flight_id):
         return render(request, "flights/single_flight.html", {
             "flight" : flight,
             "passengers" : flight.passengers.all(),
+            "non_passengers" : Passenger.objects.exclude(flights = flight).all(),
         })
     except:
         return render(request, "flights/error.html", {
             "message" : "No flight with the given id"
         })
+
+def book(request, flight_id):
+    if request.method == 'POST':
+        passenger_id = request.POST['passenger'] 
+        passenger = Passenger.objects.get(pk = passenger_id)
+        flight = Flight.objects.get(pk = flight_id)
+        passenger.flights.add(flight)
+
+        return HttpResponseRedirect(reverse('flights:single_flight', args = (flight_id,)))
