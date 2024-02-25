8866ecd6cee4245d516799d4cea36ae5957d7808
osamax01 <shkukanios@gmail.com>
2024-02-26 01:25:22 +0300

    users app: add regester view. Changes:
    1- add the view and edit urls.py
    
    2- extract the common code from regester_view and login_view to a function called login_if_valid
    
    add register and authinticate templates

--- users/views.py
+++ users/views.py
@@ -1,27 +1,34 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm 
+
+def login_if_valid(request, username, password):
+    user = authenticate(request, username = username, password = password)
+    if user is not None:
+        login(request, user)
+
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("users:login"))
+        return render(request, "users/authentication.html")
     
     return render(request, "users/welcome.html")
+
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username = username, password = password)
-        if user is not None:
-            login(request, user)
+        login_if_valid(request, username, password)
+        if request.user.is_authenticated:
             return HttpResponseRedirect(reverse("users:index"))
-        
-        return render(request, "users/login.html", {
-            "message" : "Invalid Credentials!",
-        })
+        else:  
+            return render(request, "users/login.html", {
+                "message" : "Invalid Credentials!",
+            })
 
     return render(request, "users/login.html")
 
@@ -29,4 +36,23 @@ def logout_view(request):
     logout(request)
     return render(request, "users/login.html", {
         "message" : "Logged out!",
+    })
+
+def register_view(request):
+    if (request.method == 'POST'):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            login_if_valid(request, username, password)
+            return redirect('users:index')
+        else:
+            return render(request, 'users/register.html', {
+                'form' : form
+            })
+    
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {
+        'form' : form
     })
